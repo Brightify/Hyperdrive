b8684e10b2062e5aab9410949e10f5d830b7dbe1
Jakub Turcovsky <jakub.turcovsky@brightify.org>
2021-01-20 14:13:29 +0100

    Extend string representation of RPC error.

--- krpc/annotations/src/commonMain/kotlin/org/brightify/hyperdrive/krpc/api/RPCError.kt
+++ krpc/annotations/src/commonMain/kotlin/org/brightify/hyperdrive/krpc/api/RPCError.kt
@@ -7,6 +7,10 @@ abstract class RPCError: Throwable() {
     abstract val statusCode: StatusCode
     abstract val debugMessage: String
 
+    override fun toString(): String {
+        return super.toString() + "#status = $statusCode & message = $debugMessage"
+    }
+
     @Serializable
     enum class StatusCode(val code: Int) {
         Continue(100),
