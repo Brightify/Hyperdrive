7fe0ef424efdf76e283502238a3a642fed89bf84
Jakub Turcovsky <jakub.turcovsky@brightify.org>
2021-01-20 14:12:49 +0100

    Remove unneeded annotation.

--- krpc/server/impl/src/main/kotlin/org/brightify/hyperdrive/krpc/server/impl/PingServiceDescriptor.kt
+++ krpc/server/impl/src/main/kotlin/org/brightify/hyperdrive/krpc/server/impl/PingServiceDescriptor.kt
@@ -3,9 +3,7 @@ package org.brightify.hyperdrive.krpc.server.impl
 import org.brightify.hyperdrive.krpc.PingService
 import org.brightify.hyperdrive.krpc.api.ServiceDescription
 import org.brightify.hyperdrive.krpc.api.ServiceDescriptor
-import javax.annotation.processing.Generated
 
-@Generated
 object PingServiceDescriptor: ServiceDescriptor<PingService> {
     override fun describe(service: PingService): ServiceDescription {
         return ServiceDescription("PingService", emptyList())
