9c25ed0822aee888d11a00b8e7884f8a371fecec
Jakub Turcovsky <jakub.turcovsky@brightify.org>
2021-02-15 08:18:49 +0100

    Extend RPC error list.

--- krpc/annotations/src/commonMain/kotlin/org/brightify/hyperdrive/krpc/api/RPCError.kt
+++ krpc/annotations/src/commonMain/kotlin/org/brightify/hyperdrive/krpc/api/RPCError.kt
@@ -3,7 +3,8 @@ package org.brightify.hyperdrive.krpc.api
 import kotlinx.serialization.Serializable
 
 @Serializable
-abstract class RPCError: Throwable() {
+abstract class RPCError : Throwable() {
+
     abstract val statusCode: StatusCode
     abstract val debugMessage: String
 
@@ -13,6 +14,7 @@ abstract class RPCError: Throwable() {
 
     @Serializable
     enum class StatusCode(val code: Int) {
+
         Continue(100),
         SwitchingProtocol(101),
         Processing(102),
@@ -20,7 +22,17 @@ abstract class RPCError: Throwable() {
 
         BadRequest(400),
         Unauthorized(401),
+        Forbidden(403),
         NotFound(404),
+        Gone(410),
+        UnsupportedMediaType(415),
+        ImATeapot(418),
+        UnprocessableEntity(422),
+        FailedDependency(424),
+        UpgradeRequired(426),
+        TooManyRequests(429),
+
         InternalServerError(500),
+        NotImplemented(501),
     }
 }
