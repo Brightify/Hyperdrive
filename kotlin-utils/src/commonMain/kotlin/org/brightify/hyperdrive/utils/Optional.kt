831f7e2d33e2ab59edba2e24d06b453bdd29de2d
Tadeas Kriz <tadeas@brightify.org>
2021-09-29 23:45:08 +0200

    Add Optional to kotlin-utils module.

--- kotlin-utils/src/commonMain/kotlin/org/brightify/hyperdrive/utils/Optional.kt
+++ kotlin-utils/src/commonMain/kotlin/org/brightify/hyperdrive/utils/Optional.kt
@@ -18,6 +18,35 @@ public sealed interface Optional<out T> {
     }
 }
 
+public inline fun <T: Any> Optional<T>.toKotlin(): T? {
+    return when (this) {
+        is Optional.Some -> value
+        is Optional.None -> null
+    }
+}
+
+public inline fun <T: Any> T?.toOptional(): Optional<T> {
+    return this?.let { Optional.Some(it) } ?: Optional.None
+}
+
+public inline fun <T, U> Optional<T>.map(transform: (T) -> U): Optional<U> {
+    return flatMap { Optional.Some(transform(it)) }
+}
+
+public inline fun <T, U: Any> Optional<T>.mapToKotlin(transform: (T) -> U): U? {
+    return when (this) {
+        is Optional.Some -> transform(value)
+        is Optional.None -> null
+    }
+}
+
+public inline fun <T, U> Optional<T>.flatMap(transform: (T) -> Optional<U>): Optional<U> {
+    return when (this) {
+        is Optional.Some -> transform(value)
+        is Optional.None -> Optional.None
+    }
+}
+
 @OptIn(FlowPreview::class)
 public fun <T> Flow<Optional<T>>.filterSome(): Flow<T> {
     return flatMapConcat {
