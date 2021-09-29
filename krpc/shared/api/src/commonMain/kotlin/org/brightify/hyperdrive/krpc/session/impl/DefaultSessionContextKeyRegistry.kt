54119d8aad0149ab0a116aa8d85b136f6d9e2799
Tadeas Kriz <tadeas@brightify.org>
2021-09-29 23:43:02 +0200

    Add a default implementation for SessionContextKeyRegistry.

---
+++ krpc/shared/api/src/commonMain/kotlin/org/brightify/hyperdrive/krpc/session/impl/DefaultSessionContextKeyRegistry.kt
@@ -0,0 +1,17 @@
+package org.brightify.hyperdrive.krpc.session.impl
+
+import org.brightify.hyperdrive.krpc.session.Session
+import org.brightify.hyperdrive.krpc.session.SessionContextKeyRegistry
+
+class DefaultSessionContextKeyRegistry(
+    vararg keys: Session.Context.Key<*>,
+): SessionContextKeyRegistry {
+    override val allKeys: List<Session.Context.Key<*>> = keys.toList()
+
+    private val keyMap = allKeys.associateBy { it.qualifiedName }
+
+    override fun getKeyByQualifiedName(keyQualifiedName: String): Session.Context.Key<*>? {
+        return keyMap[keyQualifiedName]
+    }
+
+}
